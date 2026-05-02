85baab6b4e4aae399642db7501ec4ef31f622d3e
Jenish Khakhkhar <khakhkharjenish4425@gmail.com>
2026-05-02 10:35:40 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+mongoose.connect("mongodb://127.0.0.1:27017/om-enterprise")
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("MongoDB Connection Error ❌", err));
+
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000 🚀");
+});
